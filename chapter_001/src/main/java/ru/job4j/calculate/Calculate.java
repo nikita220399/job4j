39d280b4331cee2eb661b09513a3692058bbdacb
nikita220399 <nik.karuk99@mail.ru>
2018-09-19 13:50:24 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
-
+/**
+* Class Calculate
+*
+* @author Nikita Karuk (nik.karuk99@mail.ru)
+* @version 1
+* @since 19.09.2018
+*/
 public class Calculate {
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
