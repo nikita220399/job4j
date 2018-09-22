271fa873fc0dffa0d0b46c3d75f7868eb6d1b85b
nikita220399 <nik.karuk99@mail.ru>
2018-09-22 15:39:06 +0300

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Nikita Karuk (nik.karuk99@mail.ru)
+* @version $Id$
+* @since 22.09.2018
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Nikita Karuk";
+        String expect = "Echo, echo, echo : Nikita Karuk"; 
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+	}	
+}
