ed115375dfbc9c7b7f8799f21a45fcd0a1464810
nikita220399 <nik.karuk99@mail.ru>
2018-10-09 19:43:11 +0300

    4.1. Глупый бот

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+/**
+ * @author Nikita Karuk
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
