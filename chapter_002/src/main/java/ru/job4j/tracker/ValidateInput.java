d69d721c90094c1665714e5f26a82176cd5917a9
nikita220399 <nik.karuk99@mail.ru>
2018-11-25 21:02:46 +0300

    Test

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,12 +1,24 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
-    public int ask (String question, int[] range) {
+public class ValidateInput implements Input {
+
+    private final Input input;
+
+    public ValidateInput(final Input input) {
+        this.input = input;
+    }
+
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
+
+    public int ask(String question, int[] range){
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
             } catch (MenuOutException moe) {
                 System.out.println("Please select key from menu.");
@@ -14,6 +26,6 @@ public class ValidateInput extends ConsoleInput {
                 System.out.println("Please enter validate data again.");
             }
         } while (invalid);
-        return value;
+        return  value;
     }
 }
